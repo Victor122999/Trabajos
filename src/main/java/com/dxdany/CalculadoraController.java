8e853fee9a65f9426ef3f401d8bf6309fb12d344
Jahir <Jahir@DESKTOP-1R9TMM2>
2022-05-19 00:43:08 -0500

    Master

--- src/main/java/com/dxdany/CalculadoraController.java
+++ src/main/java/com/dxdany/CalculadoraController.java
@@ -5,9 +5,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 
 @Controller
+@ResponseBody
 public class CalculadoraController {
 
 	
